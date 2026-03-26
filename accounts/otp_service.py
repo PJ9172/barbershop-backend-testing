9604101ebdea46d81d2616d00f418b175796328c
Prajwal <rohitjagtap5000@gmail.com>
2026-03-26 16:56:01 +0530

    add gunicorn for deployment

--- accounts/otp_service.py
+++ accounts/otp_service.py
@@ -1,60 +1,60 @@
-# import redis
-# import random
-# import os
-
-# REDIS_URL = os.getenv("REDIS_URL")
-
-# if REDIS_URL:
-#     redis_client = redis.Redis.from_url(REDIS_URL)
-# else:
-#     redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
-
-# def generate_otp():
-#     return str(random.randint(100000, 999999))
-
-
-# def save_otp(phone, otp):
-#     redis_client.setex(f"otp:{phone}", 300, otp)
-
-
-# def validate_otp(phone, otp):
-#     stored_otp = redis_client.get(f"otp:{phone}")
-
-#     if not stored_otp:
-#         return False
-
-#     if stored_otp.decode() == otp:
-#         redis_client.delete(f"otp:{phone}")
-#         return True
-
-#     return False
-
-
-
+import redis
 import random
-from datetime import datetime, timedelta
+import os
 
-otp_store = {}
+REDIS_URL = os.getenv("REDIS_URL")
+
+if REDIS_URL:
+    redis_client = redis.Redis.from_url(REDIS_URL)
+else:
+    redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
 
 def generate_otp():
     return str(random.randint(100000, 999999))
 
+
 def save_otp(phone, otp):
-    expiry_time = datetime.now() + timedelta(minutes=5)
-    otp_store[phone] = {"otp": otp, "expiry_time": expiry_time}
+    redis_client.setex(f"otp:{phone}", 300, otp)
+
 
 def validate_otp(phone, otp):
-    record = otp_store.get(phone)
+    stored_otp = redis_client.get(f"otp:{phone}")
 
-    if not record:
+    if not stored_otp:
         return False
 
-    if datetime.now() > record["expiry_time"]:
-        return False
-
-    if record["otp"] == otp:
-        del otp_store[phone]
+    if stored_otp.decode() == otp:
+        redis_client.delete(f"otp:{phone}")
         return True
 
     return False
+
+
+
+# import random
+# from datetime import datetime, timedelta
+
+# otp_store = {}
+
+# def generate_otp():
+#     return str(random.randint(100000, 999999))
+
+# def save_otp(phone, otp):
+#     expiry_time = datetime.now() + timedelta(minutes=5)
+#     otp_store[phone] = {"otp": otp, "expiry_time": expiry_time}
+
+# def validate_otp(phone, otp):
+#     record = otp_store.get(phone)
+
+#     if not record:
+#         return False
+
+#     if datetime.now() > record["expiry_time"]:
+#         return False
+
+#     if record["otp"] == otp:
+#         del otp_store[phone]
+#         return True
+
+#     return False
