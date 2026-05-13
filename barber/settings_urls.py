12ea3680a3436048faaf02f924e7aed17e6414e0
Prajwal Jagtap <rohitjagtap5000@gmail.com>
2026-05-13 11:44:32 +0530

    Url patterns change

--- barber/settings_urls.py
+++ barber/settings_urls.py
@@ -3,7 +3,7 @@ from .settings_views import *
 from .emergencyholiday_views import *
 
 urlpatterns = [
-    path('/', get_settings),
+    path('', get_settings),
     path('save/', save_settings),
     path('emergency-holiday/', get_emergency_holiday),
     path('emergency-holiday/save', save_emergency_holiday),
